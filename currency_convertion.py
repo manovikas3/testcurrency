c52b486ce5eefb4fedbf7a618f36fd9ba021a33e
manovikas3 <81741617+manovikas3@users.noreply.github.com>
2021-08-12 20:02:43 +0530

    Add files via upload
    upload complete

---
+++ currency_convertion.py
@@ -0,0 +1,21 @@
+file = 'currency.txt'
+a = open(file)
+b= a.readlines()
+#print(b)
+g = {}
+for c in b:
+    #print(c.split("\t"))
+    d = c.split("\t")
+    e = d[1]
+    f = d[0]
+    g[f] = e
+    #print(f)
+    #print(e)
+#print(g)
+print('give from the list of available currencies:\n')
+[print(h) for h in g.keys()]
+currency = input("\n enter a currency:")
+number = int(input("\n enter the INR amount: "))
+y = float(g[currency])
+z = number * y
+print(f"\n Currency conversion of INR {number} to {currency} is {z}  ")
