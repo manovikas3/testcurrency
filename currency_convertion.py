0dac1815dc5112426730af0971cdde986d38b20d
Manovikas <manovikasreddy.bussa@accolitedigital.com>
2021-08-17 13:45:24 +0530

    changed to use os variables

--- currency_convertion.py
+++ currency_convertion.py
@@ -1,28 +1,19 @@
 import os
 file = 'currency.txt'
 a = open(file)
-b= a.readlines()
-#print(b)
+b = a.readlines()
 g = {}
 for c in b:
-    #print(c.split("\t"))
     d = c.split("\t")
     e = d[1]
     f = d[0]
     g[f] = e
-    #print(f)
-    #print(e)
-#print(g)
 print('give from the list of available currencies:\n')
 [print(h) for h in g.keys()]
-# currency = input("\n enter a currency:")
-# number = int(input("\n enter the INR amount: "))
 number = os.getenv("number")
 currency = os.getenv("input_currency")
 print(currency)
 print(number)
-#currency = {currency}
-#number = {number}
 y = float(g[currency])
 z = int(number) * y
 print(f"\n Currency conversion of INR {number} to {currency} is {z}  ")
