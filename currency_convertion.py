6e9de14807a46dd4e52cbb71cb7291c84aa78acb
Manovikas <manovikasreddy.bussa@accolitedigital.com>
2021-08-24 10:06:45 +0530

    added pdf code

--- currency_convertion.py
+++ currency_convertion.py
@@ -1,3 +1,4 @@
+from fpdf import FPDF
 import os
 file = 'currency.txt'
 a = open(file)
@@ -10,10 +11,24 @@ for c in b:
     g[f] = e
 print('give from the list of available currencies:\n')
 [print(h) for h in g.keys()]
+# number = input("enter a valid INR number")
+# currency = input("enter a valid currency from the list")
 number = os.getenv("number")
 currency = os.getenv("input_currency")
 print(currency)
 print(number)
 y = float(g[currency])
 z = int(number) * y
-print(f"\n Currency conversion of INR {number} to {currency} is {z}  ")
+# print(f"\n Currency conversion of INR {number} to {currency} is {z}  ")
+output = f"\n Currency conversion of INR {number} to {currency} is {z}"
+
+# save FPDF() class into a variable pdf
+pdf = FPDF()
+# Add a page
+pdf.add_page()
+# set style and size of font that you want in the pdf
+pdf.set_font("Arial", size=15)
+# create a cell
+pdf.cell(200, 10, txt=output, ln=1, align='C')
+# save the pdf with name .pdf
+pdf.output("currency.pdf")
